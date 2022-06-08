4ff3b77a647aebba77a40bbb494a5325481e3257
Eduardo <eduardotk@hotmail.com>
2022-06-08 20:49:07 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react"
+
+function App() {
+	return (
+		<div>
+			<h1>Hello World!</h1>
+		</div>
+	)
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+	content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+	important: "#root",
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+}
